38ffcb4b9a99112b34dca89a04d129d2078afce8
Elrond-Hubbard <136862666+Elrond-Hubbard@users.noreply.github.com>
2023-10-01 00:14:11 -0400

    wrote test route controller

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
-    res.send('Hello, world!')
-})
+const pageRoutes = require('./routes')
+
+router.use('/', pageRoutes);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 
-// const routes = require('./controllers');
+const routes = require('./controllers');
 const sequelize = require('./config/connection')
 const helpers = require('./utils/helpers')
 
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use(routes);
+app.use(routes);
 
 sequelize.sync().then(() => {
     app.listen(PORT, () => console.log(`Now listening on port ${PORT}`))
