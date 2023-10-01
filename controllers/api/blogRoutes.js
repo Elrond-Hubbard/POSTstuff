b866d1cba6db45837d7cabdb1371d623e8afc7ea
Elrond-Hubbard <136862666+Elrond-Hubbard@users.noreply.github.com>
2023-10-01 15:26:53 -0400

    added additional views and styling

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -1,19 +1,6 @@
 const router = require("express").Router();
 const { User, Blog } = require("../../models/index");
 
-// Get all blogs
-router.get("/", (req, res) => {
-  Blog.findAll({ include: [User] })
-  .then((data) => res.json(data));
-});
-
-// Get one blog
-router.get("/:id", (req, res) => {
-  Blog.findOne({ include: [User], where: { id: req.params.id } })
-  .then((data) => res.json(data)
-  );
-});
-
 // Create new blog
 router.post('/', (req, res) => {
   Blog.create(req.body)
--- controllers/pageRoutes.js
+++ controllers/pageRoutes.js
@@ -1,10 +1,19 @@
 const router = require("express").Router();
 const { User, Blog } = require("../models/index");
 
+// Get homepage
 router.get("/", (req, res) => {
   Blog.findAll({ include: [User] }).then((data) => {
-    const blogs = data.map((blog) => blog.get({plain: true}))
-    res.render("home", {blogs});
+    const blogs = data.map((blog) => blog.get({ plain: true }));
+    res.render("home", { blogs });
+  });
+});
+
+// Get blog page
+router.get("/blog/:id", (req, res) => {
+  Blog.findOne({ include: [User], where: { id: req.params.id } }).then((data) => {
+    const blog = data.get({plain: true})
+    res.render('blog', blog)
   });
 });
 
