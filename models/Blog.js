df341a3329e4cb69b14baf0058baf654ccf4c07c
Elrond-Hubbard <136862666+Elrond-Hubbard@users.noreply.github.com>
2023-10-01 01:26:19 -0400

    created basic model templates

---
+++ models/Blog.js
@@ -0,0 +1,34 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Blog extends Model {}
+
+Blog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    modelName: "blog",
+  }
+);
+
+module.exports = Blog;
