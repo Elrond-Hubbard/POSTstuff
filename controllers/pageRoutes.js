a1886171ec5c2cf127ea93cfbf02f14d1607236e
Elrond-Hubbard <136862666+Elrond-Hubbard@users.noreply.github.com>
2023-10-01 11:21:44 -0400

    added basic blog view to home route

--- controllers/pageRoutes.js
+++ controllers/pageRoutes.js
@@ -1,15 +1,19 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const { User, Blog } = require("../models/index");
 
-router.get('/', (req, res) => {
-    res.render('home')
-})
+router.get("/", (req, res) => {
+  Blog.findAll({ include: [User] }).then((data) => {
+    const blogs = data.map((blog) => blog.get({plain: true}))
+    res.render("home", {blogs});
+  });
+});
 
-router.get('/dash', (req, res) => {
-    res.render('dash')
-})
+router.get("/dash", (req, res) => {
+  res.render("dash");
+});
 
-router.get('/login', (req, res) => {
-    res.render('login')
-})
+router.get("/login", (req, res) => {
+  res.render("login");
+});
 
 module.exports = router;
