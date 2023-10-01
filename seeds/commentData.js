adabf1366c26bdb52a329e6f8611f1f8e9abb608
Elrond-Hubbard <136862666+Elrond-Hubbard@users.noreply.github.com>
2023-10-01 19:41:02 -0400

    updated comment seed and site title

--- seeds/commentData.js
+++ seeds/commentData.js
@@ -2,14 +2,34 @@ const Comment = require("../models/Comment");
 
 const commentdata = [
   {
-    content: "Ahhhh!",
+    content: "The Administrator is very concerned that we get a conclusive analysis of today's sample.",
     user_id: 2,
     blog_id: 1
   },
   {
+    content: "Alright...",
+    user_id: 1,
+    blog_id: 1
+  },
+  {
+    content: "Yes, this all looks nominal.",
+    user_id: 2,
+    blog_id: 2
+  },
+  {
+    content: "I am rather looking forward to this analysis, aren't you?",
+    user_id: 1,
+    blog_id: 2
+  },
+  {
+    content: "It's not shutting down!",
+    user_id: 1,
+    blog_id: 3
+  },
+  {
     content: "Look out!",
     user_id: 3,
-    blog_id: 1
+    blog_id: 3
   },
 ];
 
